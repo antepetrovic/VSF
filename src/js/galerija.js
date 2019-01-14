e57ea5a704379d51360ec6918805475a0c89d9c0
Ante Petrovic <lelomacbook@gmail.com>
2019-01-14 17:50:54 +0100

    Optimized gallery and links

--- src/js/galerija.js
+++ src/js/galerija.js
@@ -2,7 +2,7 @@ function addImages () {
   for(let i = 1; i < 29; i++) {
     $('#lightgallery').append(
     `<a href="../src/img/falcon-slike/${i}.jpg">
-      <img src="../src/img/falcon-slike/${i}.jpg" />
+      <img src="../src/img/falcon-thumbnails/${i}.jpg" />
     </a>`)
   }
 }
