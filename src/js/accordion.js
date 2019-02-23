16a68057bbd671d36dcd2c9d0c12335979d5d3d3
Ante Petrovic <lelomacbook@gmail.com>
2019-02-23 20:34:30 +0100

    changed medicina rada

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,7 +1,6 @@
 let acc = document.getElementsByClassName("accordion");
-let i;
 
-for (i = 0; i < acc.length; i++) {
+for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
