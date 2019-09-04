57ae1059bd547a21e83ffb8825ef84e257c5e741
Ante Petrovic <lelomacbook@gmail.com>
2019-09-04 10:33:10 +0200

    Falcon izmjene

--- src/js/galerija.js
+++ src/js/galerija.js
@@ -1,8 +1,8 @@
 function addImages () {
-  for(let i = 1; i < 28; i++) {
+  for(let i = 1; i < 33; i++) {
     $('#lightgallery').append(
     `<a href="../src/img/falcon-slike/${i}.jpg">
-      <img class="grow" src="../src/img/falcon-thumbnails/${i}.jpg" />
+      <img class="grow" src="../src/img/falcon-thumbnails/${i}.jpg" alt='falcon-peregrin-izokinetika' />
       <div class="poster">
       
       </div>
